fb4c0f28093d40ea7a6d69e7a3aee75ef7164e9b
Émilia Gagné <202080225@edu.clg.qc.ca>
2022-05-08 14:40:39 -0400

    Création script test émilia

---
+++ Bruit_Perlin/EmiliaTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+struct Point
+{
+	double x;
+	double y;
+}
+public class Class1
+{
+	
+	public Class1()
+	{
+
+
+	}
+	void perlin(double x, double y)
+    {
+		Point pointDépart = new Point(x, y);
+    }
+
+	float[] TrouverCoins(Point p)
+    {
+		
+    }
+}
