83481edec276aae4235e52423ca3eff6fd527a10
Nathan Bougie <202046701@edu.clg.qc.ca>
2022-05-08 14:40:06 -0400

    Ajout point et Vector2 et Vector3

---
+++ Bruit_Perlin/Vector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Vector3
+{
+    public Point point { get; private set; }
+
+    public Vector3(double x, double y, double z)
+    {
+        point = new Point(x, y, z);
+    }
+
+}
+
+public class Vector2
+{
+    Point point { get; private set; }
+    public Vector2(double x, double y)
+    {
+        point = new Point(x, y);
+    }
+
+}
+
+struct Point
+{
+    public double X, Y;
+    public double Z = 0;
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public Point(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
